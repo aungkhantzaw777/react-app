761b97078c7eda5876b89ca553c52c1ef04ff25b
aung khant zaw <aungkhantzaw133@gmail.com>
2019-03-16 16:11:49 +0630

    add name ok

--- src/App.js
+++ src/App.js
@@ -1,28 +1,51 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Item from './item.js';
+import Add from './Add.js';
 
-class App extends Component {
-  render() {
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      data:[
+        'boo',
+        'alice',
+        'jonny',
+        'mary'
+      ]
+    }
+    this.input = React.createRef();
+    this.add = this.add.bind(this);
+  }
+
+  add(name){
+    var data = this.state.data;
+    // var name = this.input.current.value;
+    data.push(name);
+
+    this.setState({
+      data:data
+    });
+  }
+  render(){
+    var data = ['bob', 'Alice', 'tom', 'jonny'];
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+      
+      <ul>
+        {data.map(item => <Item name={item} />)}
+        
+      </ul>
+
+      <ul>
+        {this.state.data.map(item => <Item name={item} />)}
+      </ul>
+      <Add add={this.add} />
       </div>
+      
     );
   }
 }
+
+
 
 export default App;
